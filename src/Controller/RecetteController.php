fd99fa77647b1bd47fe00ddda6ded36719063620
ValentinYgout <63228068+ValentinYgout@users.noreply.github.com>
2020-05-07 20:28:42 +0200

    Recette form
    Recette form

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -15,15 +15,18 @@ class RecetteController extends AbstractController
     /**
      * @Route("/recette", name="recette")
      */
-    public function index()
-    {
+    public function index(RecetteRepository $ar)
+    { 
+        $liste_recettes = $ar->findAll();
         return $this->render('recette/index.html.twig', [
             'controller_name' => 'RecetteController',
+            'liste_recettes' => $liste_recettes,
         ]);
     }
 
     /**
      * @Route("/recette/new", name="ajouter_recette")
+     * 
      */
     public function ajouter_recette(Request $request, EntityManager $em)
     {
@@ -58,6 +61,61 @@ class RecetteController extends AbstractController
         ]);
 
         
+    }
+     /**
+     * @Route("/fiche/recette/{id}", name="recette_fiche", requirements={"id"="\d+"})
+     * à cause de requirements, id doit être composé d'1 ou plusieurs chiffres 
+     */
+    public function fiche(RecetteRepository $ar, $id)
+    {
+        $recette = $ar->find($id);
+        if(!empty($recette)){
+            return $this->render("recette/fiche.html.twig", [ "recette" => $recette ]);
+        }
+        return $this->redirectToRoute("recette");
+       
+    }
+
+    /**
+     * @Route("/recette/modifier/{id}", name="recette_modifier", requirements={"id"="\d+"})
+     */
+    public function modifier(Request $rq, EntityManager $em, RecetteRepository $ar, $id)
+    {
+        // Modifier un recette existant
+        // récupérer l'recette avec son id, l'afficher dans le formulaire et enregistrer les modifications dans la base de données
+        $recetteAmodifier = $ar->find($id);
+        $formRecette = $this->createForm(RecetteType::class, $recetteAmodifier);
+        $formRecette->handleRequest($rq);
+        if($formRecette->isSubmitted() && $formRecette->isValid()){
+            $em->persist($recetteAmodifier);
+            $em->flush();
+            $this->addFlash("success", "Les informations de l'recette ont été modifiées");
+            return $this->redirectToRoute("recette");
+        }
+        return $this->render("recette/formRecette.html.twig", [ 
+            "formRecette" => $formRecette->createView(), 
+            "bouton" => "Modifier",
+            "titre" => "Modification de l'recette n°$id" 
+        ]);
+    }
+
+    /**
+     * @Route("/recette/supprimer/{id}", name="recette_supprimer", requirements={"id"="\d+"})
+     */
+    public function supprimer(Request $rq, EntityManager $em, RecetteRepository $recette, $id)
+    {
+       
+        $recette = $recette->find($id);
+        $nomrecette = $recette->getTitre();
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($recette);
+        $em->flush();
+
+        $this->addFlash('danger', "L'utilisateur  \"$nomrecette\"  a bien été supprimé!");
+
+        return $this->redirectToRoute("liste_recette");
+       
     }
 
 }
