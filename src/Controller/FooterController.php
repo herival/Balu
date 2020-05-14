0a57efe29620765346011c6bc3acc4cd9d00fce6
Heri <herimalala.val@gmail.com>
2020-05-14 18:17:27 +0200

    form contact

--- src/Controller/FooterController.php
+++ src/Controller/FooterController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FooterController extends AbstractController
 {
@@ -28,13 +31,37 @@ class FooterController extends AbstractController
         /**
      * @Route("/footer/contact", name="contact")
      */
-    public function contact()
+    public function contact(Request $request, \Swift_Mailer $mailer)
     {
+        $formContact = $this->createForm(ContactType::class);
+        $formContact->handleRequest($request);
+
+        if($formContact->isSubmitted() && $formContact->isValid()){
+            $contact = $formContact->getData();
+            // dd($contact);
+            $message = (new \Swift_Message('Nouveau Contact'))
+                ->setFrom($contact['email'])
+                ->setTo('herimalala.val@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'emails/contact.html.twig', compact('contact')
+                    ),
+                    'text/html'
+                )
+            ;
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Le message à bien été envoyé');
+
+
+            return $this->redirectToRoute('contact');
+        }
+
         return $this->render('footer/contact.html.twig', [
-            'controller_name' => 'contact',
+            'formContact' => $formContact->createView()
         ]);
     }
-        /**
+    /**
      * @Route("/footer/mentionslegales", name="mentionslegales")
      */
     public function mentionslegales()
